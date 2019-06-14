a7af29ef2f3fd8f6d01374e23d175ed6c905fd9c
Jsollozo <jr.sollozo@gmail.com>
2019-06-13 21:38:53 -0700

    Pause menu
    -made pause menu
    -script needs a little work

---
+++ Competitive/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public GameObject controlsPanel;
+    public GameObject instructions;
+
+    private bool m_IsPaused = false;
+
+    void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            controlsPanel.SetActive(true);
+            TogglePause();
+        }
+        else if (Input.GetButtonDown("Resume"))
+        {
+            TogglePause();
+        }
+        else if (Input.GetButtonDown("Instruction"))
+        {
+            ToggleInstructions();
+        }
+        else if (Input.GetButtonDown("Quit"))
+        {
+            ToggleQuit();
+        }
+    }
+
+    private void TogglePause()
+    {
+        m_IsPaused = !m_IsPaused;
+
+        if (m_IsPaused)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+
+    private void ToggleInstructions()
+    {
+        instructions.SetActive(true);
+    }
+
+    private void ToggleQuit()
+    {
+        Application.Quit();
+    }
+
+    public void Open()
+    {
+        controlsPanel.SetActive(true);
+        TogglePauseOnPlayers();
+    }
+
+    public void Close()
+    {
+        controlsPanel.SetActive(false);
+        TogglePauseOnPlayers();
+    }
+
+    private void TogglePauseOnPlayers()
+    {
+        var players = GameObject.FindGameObjectsWithTag("Player");
+
+        foreach (var player in players)
+        {
+            player.GetComponent<PlayerBehaviour>().TogglePause();
+        }
+    }
+}
